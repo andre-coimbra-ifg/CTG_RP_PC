166b9e8031660f9d347262f97616400f0be87eb5
Andre Coimbra <andrecoimbra@MacBook-Pro-de-Andre.local>
2023-12-15 10:52:10 -0300

    Testing PC image

--- src/libPC.py
+++ src/libPC.py
@@ -12,7 +12,9 @@ import imageio
 nni = pyhrv.utils.load_sample_nni()
 
 # Compute Poincaré using NNI series
-results = nl.poincare(nni)
+# results = nl.poincare(nni)
+results = nl.poincare(nni, ellipse=False, vectors=False, legend=False)
+
 
 # Print SD1
 print(results['sd1'])
